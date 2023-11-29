3824097264f3e77cfd32796243463362f4107d7d
DaBetterman <1ebrahimr@gmail.com>
2023-11-29 15:01:51 +0200

    update task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 def fizzbuzz():
-        j = 1
-        k = 1
-        for i in range(1, 101):
-            if i == (3 * j) and i == (5 * k):
-                print("FizzBuzz", end=" ")
-                k = k + 1
-                j = j + 1
-            elif i == (3 * j):
-                  print("Fizz", end=" ")
-                  j = j + 1
-            elif i == (5 * k):
-                  print("Buzz", end=" ")
-                  k = k + 1
-            else:  
-                print("{}".format(i), end=" ")
+    j = 1
+    k = 1
+    for i in range(1, 101):
+        if i == (3 * j) and i == (5 * k):
+            print("FizzBuzz", end=" ")
+            k = k + 1
+            j = j + 1
+        elif i == (3 * j):
+            print("Fizz", end=" ")
+            j = j + 1
+        elif i == (5 * k):
+            print("Buzz", end=" ")
+            k = k + 1
+        else:
+            print("{}".format(i), end=" ")
