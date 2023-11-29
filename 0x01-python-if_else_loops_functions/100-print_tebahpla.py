91dec78d5b366b443e843aee7f507ea9b0c886fa
DaBetterman <1ebrahimr@gmail.com>
2023-11-29 19:35:23 +0200

    update task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
 for i in range(122, 96, -1):
-    print("{}".format(chr(i).upper() if i % 2 == 0 else chr(i)), end="")
+    print("{}".format(chr(i).upper() if i % 2 == 1 else chr(i)), end="")
+print("{}".format(chr(i).upper() if i % 2 == 0 else chr(i)), end="")
