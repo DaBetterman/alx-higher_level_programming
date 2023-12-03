c3b4558078b5907dc41c239e9d6d5f42adff1ac7
DaBetterman <1ebrahimr@gmail.com>
2023-12-03 21:51:45 +0200

    task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return
+    
+    for number in matrix:
+        for i in range(len(number)):
+            print("{:d}".format(number[i]), end="")
+            if i != len(number) - 1:
+                print(" ", end="")
+        print()
