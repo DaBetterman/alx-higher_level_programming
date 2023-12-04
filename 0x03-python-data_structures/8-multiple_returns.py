77802ab1ccb84a599c7ee6cb08ab34e40faa12d5
DaBetterman <1ebrahimr@gmail.com>
2023-12-04 12:08:18 +0200

    task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,7 @@
 def multiple_returns(sentence):
     count = len(sentence)
     if count == 0:
-        count = None
-    first_letter = sentence[0]
+        first_letter = None
+    else:
+        first_letter = sentence[0]
     return (count, first_letter)
