ae33b4f56a80b066b60d9d9383b9b7ffa5501bc7
DaBetterman <1ebrahimr@gmail.com>
2023-12-06 22:44:56 +0200

    task 9

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def best_score(a_dictionary):
+    count = 0
+    best_score = None
+    if type(a_dictionary) is dict:
+        for (key, value) in a_dictionary.items():
+            if value > count:
+                count = value
+                best_score = key
+    return best_score
---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+def convert_roman(ch):
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                  'C': 100, 'D': 500, 'M': 1000}
+    return (roman_dict.get(ch, -1))
+
+
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return (0)
+    
+    conv = 0
+    prev_value = 0
+
+    for i in reversed(roman_string):
+        cur = convert_roman(i)
+        
+        if cur == -1:
+            return (0)
+        
+        if cur < prev_value:
+            conv -= cur
+        else:
+            conv += cur
+            
+        prev_value = cur
+
+    return (conv)
