b01d7066afa13f2017af619c97e468dcd09b3532
DaBetterman <1ebrahimr@gmail.com>
2023-12-18 20:31:30 +0200

    task 101

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 
 def safe_function(fct, *args):
+    import sys
+    try:
+        result = fct(*args)
+        return result
+    except Exception as message:
+        sys.stderr.write("Exception: {}\n".format(message))
+        return None
     
