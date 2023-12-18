e6a510c3dfc669381ee4689eb92a6da9fe22a867
DaBetterman <1ebrahimr@gmail.com>
2023-12-18 14:34:50 +0200

    task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
-    
+    try:
+        if isinstance(value, int):
+            print("{:d}".format(value))
+            return (value)
+    except:
+        print("str")
