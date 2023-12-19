79a10b8130e453dc87298ce22a4563b818c7a270
DaBetterman <1ebrahimr@gmail.com>
2023-12-19 20:18:18 +0200

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    This is the Square class.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        No methods at the moment.
+    """
+
+    def __init__(self, size):
+        self.__size = size
+
