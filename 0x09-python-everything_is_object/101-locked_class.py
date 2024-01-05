9a8a96b3547081e858cf88967bbcb8e49be56a3f
DaBetterman <1ebrahimr@gmail.com>
2024-01-05 19:35:02 +0200

    task 30

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,5 @@
-#!/usr/bin/python3
-
-
 class LockedClass:
-    """Locked class: can't set instance attributes to it
+    """Locked class: can't set instance attributes except 'first_name'
     """
     def __setattr__(self, name, value):
         if name == "first_name":
