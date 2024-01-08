1bf2e0300d0868213d683be3d78bc2aa5e534455
DaBetterman <1ebrahimr@gmail.com>
2024-01-08 16:29:23 +0200

    Update task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
+
+
 def lookup(obj):
+    """returns all objects in an objects dictionary
+        -> as a list
     """
-    a function that returns the list of available
-    attributes and methods of an object
-    """
-    if not isinstance(obj, list):
-        return None
-    else:
-        return dir(obj)
+    return dir(obj)
