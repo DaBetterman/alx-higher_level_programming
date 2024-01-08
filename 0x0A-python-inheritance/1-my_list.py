2b197d9992a3208e71ac33f563a835cbbe1f771c
DaBetterman <1ebrahimr@gmail.com>
2024-01-08 17:03:07 +0200

    Update task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,5 +4,8 @@ class MyList(list):
     Write a class MyList that inherits from list.
     """
     def print_sorted(self):
-        inherited_list = sorted(self)
-        print(inherited_list)
+        if isinstance(self, list):
+            inherited_list = sorted(self)
+            print(inherited_list)
+        else:
+            return None
