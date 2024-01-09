ef9e3b5a5063096b1202805072dc39b4f5477c04
DaBetterman <1ebrahimr@gmail.com>
2024-01-09 16:28:48 +0200

    task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+
+"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    
+    """
+    with open(filename, "w") as new_file:
+        json.dump(my_obj, new_file)
