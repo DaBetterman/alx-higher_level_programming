4ceed8a050c5505e5fbb2ae2f135d45ea721b9a4
DaBetterman <1ebrahimr@gmail.com>
2024-01-09 13:13:35 +0200

    task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This script defines a function for writing text to a file.
+"""
+def write_file(filename="", text=""):
+    """
+    Write the given text to the specified file.
+
+    Args:
+        filename: The name of the file to write to.
+        text: The text to be written to the file.
+    Return:
+        The number of bytes written to the file.
+    """
+    with open(filename, "w") as new_file:
+        return new_file.write(text)
