5b35bac9a5b1c47e32f11980db667d3720cb341e
DaBetterman <1ebrahimr@gmail.com>
2024-02-15 19:52:53 +0200

    task 19 & 20

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,7 @@
+-- script for selecting TV shows and their genres
+-- join statement to do result DML
+SELECT title, SUM(tv_show_ratings.rate) 'rating'
+FROM tv_shows
+LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY title
+ORDER BY rating DESC;
