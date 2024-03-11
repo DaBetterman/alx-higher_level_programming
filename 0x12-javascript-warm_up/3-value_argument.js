c815af8b585d4bf9a9185e4996aa69cbd7038611
DaBetterman <1ebrahimr@gmail.com>
2024-03-11 17:10:00 +0200

    task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
 
-const [, , argv] = process.argv;
+const argv = process.argv;
+let i = 2;
+let bool = false;
 
-if (argv) {
-  console.log(argv);
-} else {
+for (i = 2; argv[i] !== undefined; i++) {
+  console.log(argv[i]);
+  bool = true;
+}
+if (!bool) {
   console.log('No arguments');
 }
