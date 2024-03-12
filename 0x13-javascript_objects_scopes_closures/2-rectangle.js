d83b156f8d68582f58cfaa210070a0a2e5494140
DaBetterman <1ebrahimr@gmail.com>
2024-03-12 16:40:01 +0200

    task 2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+class Rectangle {
+  constructor (w, h) {
+	  if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+      return 'Rectangle {}';
+	  } else {
+      this.width = w;
+      this.height = h;
+	  }
+  }
+}
+
+module.exports = Rectangle;
