a16087448cec5ba60dc83a4f90affcc9ca4fe6d3
DaBetterman <1ebrahimr@gmail.com>
2024-03-12 21:39:37 +0200

    update syntax

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,11 +3,10 @@
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
   let i = 0;
-
   for (i = 0; i < list.length; i++) {
-	  if (list[i] === searchElement) {
+    if (list[i] === searchElement) {
       count++;
-	  }
+    }
   }
   return count;
 };
