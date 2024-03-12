6eb1484af6901d0992bb3f358aab354d087e96c9
DaBetterman <1ebrahimr@gmail.com>
2024-03-12 07:08:35 +0200

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,3 +1,10 @@
 #!/usr/bin/node
 
-const argv = parseInt(process.argv);
+const argv = process.argv.slice(2).map(Number);
+
+if (argv.length <= 1) {
+  console.log(0);
+} else {
+  const sortedArgv = argv.sort((a, b) => b - a);
+  console.log(sortedArgv[1]);
+}
