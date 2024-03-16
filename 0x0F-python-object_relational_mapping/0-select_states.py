fd702e6a32215117b77f2cd83f51038881c0d049
DaBetterman <1ebrahimr@gmail.com>
2024-03-16 16:11:54 +0200

    task 1 & task 0 update

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,11 +6,26 @@ lists all states from the database hbtn_0e_0_usa
                             <database name >
 """
 
+import MySQLdb
 import sys
-import mysql.connector as MySQLdb
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall()]
+db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], password=sys.argv[2], database=sys.argv[3])
+
+cursor = db.cursor()
+cursor.execute("SELECT * FROM states")
+
+rows = cursor.fetchall()
+
+for row in rows:
+	print(row)
+
+cursor.close()
+db.close()
+
+
+"""
+MySQLdb
+to connect: (host, port, user, password, database) into a database variable (db)
+db.cursor
+rows = cursor.fetchall()
+"""
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,12 +7,16 @@ lists all states with a name starting with N (upper N)
                             <database name>
 """
 
-import sys
-from unicodedata import name
 import MySQLdb
+import sys
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states` ORDER BY `id`")
-    [print(state) for state in c.fetchall() if state[1][0] == "N"]
+db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], password=sys.argv[2], database=sys.argv[3])
+cursor = db.cursor()
+cursor.execute('SELECT * FROM states WHERE name LIKE "N%"')
+rows = cursor.fetchall()
+
+for row in rows:
+	print(row)
+
+cursor.close()
+db.close()
