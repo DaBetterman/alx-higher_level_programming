afe6bc21c3935106eb7f2fb43630b9589e1f9a2b
DaBetterman <1ebrahimr@gmail.com>
2024-03-16 16:19:39 +0200

    added if name == main

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,24 +10,11 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-
-	db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], password=sys.argv[2], database=sys.argv[3])
-
+	db = MySQLdb.connect(user=sys.argv[1], password=sys.argv[2], database=sys.argv[3])
 	cursor = db.cursor()
 	cursor.execute("SELECT * FROM states")
-
 	rows = cursor.fetchall()
-
 	for row in rows:
 		print(row)
-
 	cursor.close()
 	db.close()
-
-
-"""
-MySQLdb
-to connect: (host, port, user, password, database) into a database variable (db)
-db.cursor
-rows = cursor.fetchall()
-"""
