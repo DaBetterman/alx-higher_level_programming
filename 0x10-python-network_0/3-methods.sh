2ff62c43b3fc0e33c93e59e073e97efcb39e822b
dabetterman <1ebrahimr@gmail.com>
2024-03-27 13:52:15 -0700

    added the X flag

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # A Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sI "$1" | grep -i "Allow" | awk -F ": " '{ print $2 }'
+curl -s -I -X OPTIONS "$1" | grep 'Allow:' | cut -f2- -d' '
 
