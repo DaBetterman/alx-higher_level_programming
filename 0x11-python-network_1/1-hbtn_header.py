a4c0c0e3b468657a75ce204c059618ba086413de
DaBetterman <1ebrahimr@gmail.com>
2024-03-28 17:18:38 +0200

    update task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,6 +2,7 @@
 from sys import argv
 import urllib.request
 
+
 def main(argv):
     """
     A Python script that takes in a URL, sends a request to
@@ -12,6 +13,7 @@ def main(argv):
     with urllib.request.urlopen(url) as file:
         headers = file.info()
         print(headers['X-Request-Id'])
+
 
 if __name__ == "__main__":
     main(argv[1])
